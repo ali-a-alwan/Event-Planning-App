8af8039c9459cd1ac3199c97bf4035c2df9e27a9
ali-a-alwan <156842868+ali-a-alwan@users.noreply.github.com>
2024-03-29 15:14:09 -0400

    Add files via upload

---
+++ EventDetailsScreen.js
@@ -0,0 +1,31 @@
+// EventDetailsScreen.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+});
+
+export default EventDetailsScreen;
